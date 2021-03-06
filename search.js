7953b433232db45bc5e7278f0b16080009aeefef
Yugal-Kishor <79036512+Yugal-Kishor@users.noreply.github.com>
2021-03-06 18:21:59 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,69 +1,53 @@
-$(document).ready(function(){
-  var API = "AIzaSyAJKu7caQTAYm3JpF4S6RKt6pESZr0kNV4"
-  $(".searching").submit(function(event){
-      event.preventDefault()
-      var search = $("#search").val()
-      videoSearch(API,search,12)
-  })
-  function videoSearch(key,search,maxResults){
-      $("#videos").empty();
-
-      $.get("https://www.googleapis.com/youtube/v3/search?key="+key+"&tye=video&part=snippet&maxResults="+maxResults+"&q="+search,function(data){
-          console.log(data)
-          data.items.forEach(item => {
-              video = `
-              <iframe width="420" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen><iframe>
-              `
-              $("#videos").append(video)
-          });
-      })
-  }
-})
+// User Authentication Sign In and Sign 
 
 
-// firebase.auth().onAuthStateChanged(function(user) {
-//     if (user) {
-//       // User is signed in.
-//       document.getElementById("userdiv").style.display="flex" ;
-//       document.getElementById("logindiv").style.display="none";
-//       console.log("signed in")
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      document.getElementById("userdiv").style.display="default" ;
+      document.getElementById("logindiv").style.display="none";
+      console.log("signed in");
   
-//     } else {
-//       // No user is signed in.
-//       document.getElementById("userdiv").style.display="none" ;
-//       document.getElementById("logindiv").style.display="flex";
-//       console.log("Please Sign in")
+    } else {
+      // No user is signed in.
+      document.getElementById("userdiv").style.display="default" ;
+      document.getElementById("logindiv").style.display="default";
+      console.log("Please Sign in");
   
-//     }
-//   });
+    }
+  });
 
-// function signin(){
-//     var userEmail = document.getElementById("email1").value;
-//     var userPass = document.getElementById("password1").value;
+function signin(){
+    var userEmail = document.getElementById("email1").value;
+    var userPass = document.getElementById("password1").value;
+    
 
-//     firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
-//     .then((userCredential) => {
-//       // Signed in
-//       var user = userCredential.user;
-//       // ...
-//     })
-//     .catch((error) => {
-//       var errorCode = error.code;
-//       var errorMessage = error.message;
-//       window.alert("Error : " +errorMessage)
-//     });
-// }
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log(userCredential);
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error : " +errorMessage )  ;
+    });
+}
 
- function OpenSignIn(){
-     document.getElementById("SignIn").style.display="flex" ;
-     document.getElementById("SignUp").style.display="none";
+function OpenSignIn(){
+    document.getElementById("SignIn").style.display="flex" ;
+    document.getElementById("SignUp").style.display="none";
 
- }
- function OpenSignUp(){
-     document.getElementById("SignIn").style.display="none";
-     document.getElementById("SignUp").style.display="flex";
+}
+function OpenSignUp(){
+    document.getElementById("SignIn").style.display="none";
+    document.getElementById("SignUp").style.display="flex";
 
- }
+}
+
+// Speech Recognition (Voice Search)
 
 const searchForm = document.querySelector(".searching");
 const searchFormInput = document.querySelector("#search");
@@ -73,12 +57,12 @@ if (SpeechRecognition) {
   console.log("browser support speech recognition")
 
   const micBtn = searchForm.querySelector("button");
-  const micIcon = micBtn.querySelector("img");
+  const micIcon = micBtn.querySelector("i");
 
   const recognition = new SpeechRecognition();
   micBtn.addEventListener("click", micBtnClick);
   function micBtnClick(){
-    if (micIcon.classList.contains("vostart")) {// Start Speech Recognition
+    if (micIcon.classList.contains("fa-microphone")) {// Start Speech Recognition
       recognition.start();
       
     } 
@@ -90,16 +74,16 @@ if (SpeechRecognition) {
   }
   recognition.addEventListener("start", startSpeechRecognition);
   function startSpeechRecognition() {
-    micIcon.classList.remove("vostart");
-    micIcon.classList.add("voend");
+    micIcon.classList.remove("fa-microphone");
+    micIcon.classList.add("fa-microphone-alt");
     searchFormInput.focus();
     console.log("Speech Recognition Active");
       
   }
   recognition.addEventListener("end", endSpeechRecognition);
   function endSpeechRecognition() {
-    micIcon.classList.remove("voend");
-    micIcon.classList.add("vostart");
+    micIcon.classList.remove("fa-microphone-alt");
+    micIcon.classList.add("fa-microphone");
     searchFormInput.focus();
     console.log("Speech Recognition not Active");
   }
@@ -120,4 +104,29 @@ if (SpeechRecognition) {
   console.log("browser does not support speech recognition")
   
 }
+
+// YouTube API
+
+$(document).ready(() => {
+  var API = "AIzaSyAJKu7caQTAYm3JpF4S6RKt6pESZr0kNV4"
+  $(".searching").submit(function (event) {
+    event.preventDefault()
+    var search = $("#search").val()
+    videoSearch(API, search, 15)
+  })
+  function videoSearch(key, search, maxResults) {
+    $("#videos").empty()
+
+    $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&tye=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function (data) {
+      console.log(data)
+      data.items.forEach(item => {
+        video = `
+            <iframe width="420" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen><iframe>
+            `
+        $("#videos").append(video)
+      })
+    })
+  }
+})
+
 
